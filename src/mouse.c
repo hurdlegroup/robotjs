7d82481a76a6181512b8b55892eb0d21094c76a8
Daniel McAssey <DanielMcAssey@users.noreply.github.com>
2024-05-09 17:29:55 +0100

    fix(robotjs): fix vertical scroll on Windows, fixes #20 (#21)

--- src/mouse.c
+++ src/mouse.c
@@ -325,11 +325,11 @@ void scrollMouse(int x, int y)
 
 #elif defined(IS_WINDOWS)
 
-	// Must send y first, otherwise we get stuck when scrolling on y axis
+	// Must send x first, otherwise we get stuck when scrolling on y axis
 	mouseScrollInputs[0].type = INPUT_MOUSE;
 	mouseScrollInputs[0].mi.dx = 0;
 	mouseScrollInputs[0].mi.dy = 0;
-	mouseScrollInputs[0].mi.dwFlags = MOUSEEVENTF_WHEEL;
+	mouseScrollInputs[0].mi.dwFlags = MOUSEEVENTF_HWHEEL;
 	mouseScrollInputs[0].mi.time = 0;
 	mouseScrollInputs[0].mi.dwExtraInfo = 0;
 	// Flip x to match other platforms.
@@ -338,7 +338,7 @@ void scrollMouse(int x, int y)
 	mouseScrollInputs[1].type = INPUT_MOUSE;
 	mouseScrollInputs[1].mi.dx = 0;
 	mouseScrollInputs[1].mi.dy = 0;
-	mouseScrollInputs[1].mi.dwFlags = MOUSEEVENTF_HWHEEL;
+	mouseScrollInputs[1].mi.dwFlags = MOUSEEVENTF_WHEEL;
 	mouseScrollInputs[1].mi.time = 0;
 	mouseScrollInputs[1].mi.dwExtraInfo = 0;
     mouseScrollInputs[1].mi.mouseData = y;
